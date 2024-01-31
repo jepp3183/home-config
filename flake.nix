bd236291858eb8d4ab09583abc2ce59d846dca67
Jeppe Allerslev <jeppeallerslev@gmail.com>
2024-01-31 23:13:51 +0100

    init commit

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Home Manager configuration of jeppe";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+   hyprland = {
+      url = "github:hyprwm/hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+   };
+
+    
+    nix-colors.url = "github:misterio77/nix-colors";
+  };
+
+  outputs = { nixpkgs, home-manager, ... } @ inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      homeConfigurations."jeppe" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        extraSpecialArgs = {
+          inherit inputs;
+        };
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };
+}
