0d62750cebba2fa0b9bb8a507c19069f8347a4bb
Jeppe Allerslev <jeppeallerslev@gmail.com>
2024-02-11 23:33:23 +0100

    fixed some indendation settings

--- configs/neovim/default.nix
+++ configs/neovim/default.nix
@@ -14,10 +14,16 @@ in
     vim.opt.number = true
     vim.opt.cursorline = true
     vim.opt.relativenumber = true
+
     vim.opt.tabstop = 4
     vim.opt.shiftwidth = 4
+    vim.opt.softtabstop = 4
     vim.opt.expandtab = true
     vim.opt.autoindent = true
+    vim.opt.smartindent = true
+
+
+    vim.opt.incsearch = true
     vim.opt.termguicolors = true
     vim.opt.hidden = true
     vim.opt.splitright = true
