931a72a90047c4c27de26fe57da655718be08092
Jeppe Allerslev <jeppeallerslev@gmail.com>
2024-03-03 22:18:56 +0100

    added zellij commands to locked mode

--- configs/zellij.nix
+++ configs/zellij.nix
@@ -8,10 +8,23 @@
   home.file.".config/zellij/config.kdl".text = ''
     pane_frames false
 
+
     keybinds {
       unbind "Ctrl q"
-      normal {
+      shared {
         bind "Alt t" {ToggleFloatingPanes;}
+        bind "Alt n" { NewPane; }
+        bind "Alt i" { GoToPreviousTab; }
+        bind "Alt o" { GoToNextTab; }
+        bind "Alt h" "Alt Left" { MoveFocusOrTab "Left"; }
+        bind "Alt l" "Alt Right" { MoveFocusOrTab "Right"; }
+        bind "Alt j" "Alt Down" { MoveFocus "Down"; }
+        bind "Alt k" "Alt Up" { MoveFocus "Up"; }
+        bind "Alt =" "Alt +" { Resize "Increase"; }
+        bind "Alt -" { Resize "Decrease"; }
+        bind "Alt [" { PreviousSwapLayout; }
+        bind "Alt ]" { NextSwapLayout; }
+        bind "Alt x" { CloseFocus; }
       }
     }
   '';
