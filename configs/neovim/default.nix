95c200562fcc8b9dcbacac41293d648f5a7da9b6
Jeppe Allerslev <jeppeallerslev@gmail.com>
2024-05-30 22:21:19 +0200

    add back toggleterm!

--- configs/neovim/default.nix
+++ configs/neovim/default.nix
@@ -61,7 +61,19 @@ in
     { plugin = nvim-autopairs; config = toLua ''require("nvim-autopairs").setup()''; }
     { plugin = comment-nvim; config = toLua ''require("Comment").setup()''; }
     { plugin = gitsigns-nvim; config = toLua ''require("gitsigns").setup()''; }
-    { plugin = toggleterm-nvim; config = toLua ''require('toggleterm').setup{} ''; }
+    { plugin = toggleterm-nvim; config = toLua ''
+       require('toggleterm').setup{
+        open_mapping = [[<c-t>]],
+        size =  function(term)
+          if term.direction == "horizontal" then
+            return 20
+          elseif term.direction == "vertical" then
+            return vim.o.columns * 0.4
+          end
+        end,
+        direction = 'float'
+      }
+    ''; }
 
     { plugin = copilot-lua; config = toLua ''
         require('copilot').setup({
