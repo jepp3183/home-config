5c029af40273e69874b459c274c2ed8fbdfdac85
Jeppe Allerslev <jeppeallerslev@gmail.com>
2024-06-12 10:10:31 +0200

    alias

--- configs/fish.nix
+++ configs/fish.nix
@@ -51,8 +51,6 @@ in
       dpsa = "docker ps -a --format=\"table {{.Names}}\t{{.Image}}\t{{.Status}}\"";
       lfd = "cd (lf -print-last-dir)";
       y = "yazi";
-      ll = "eza -a --icons --git --long --group-directories-first";
-      lt = "eza --icons --git --long --tree --level=2 --group-directories-first";
     };
     shellAbbrs = {
       nse = "nix-search -d";
@@ -60,6 +58,8 @@ in
       wgd = "wg-quick down";
       nv = "nvim";
       gs = "git status";
+      ll = "eza -a --icons --git --long --group-directories-first";
+      lt = "eza --icons --git --long --tree --group-directories-first --level=2 ";
     };
     interactiveShellInit = ''
       set fish_greeting
