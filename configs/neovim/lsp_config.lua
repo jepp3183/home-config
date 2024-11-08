7ca3f11985eafaad5a1ff7c0f828b20e7c593dba
Jeppe Allerslev <jeppeallerslev@gmail.com>
2024-11-08 20:23:47 +0100

    fix snipping and completion integration

--- configs/neovim/lsp_config.lua
+++ configs/neovim/lsp_config.lua
@@ -58,11 +58,6 @@ require 'lspconfig'.lua_ls.setup {
   end
 }
 
-local has_words_before = function()
-  unpack = unpack or table.unpack
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
 local cmp = require('cmp')
 local luasnip = require('luasnip')
 -- If you want insert `(` after select function or method item
@@ -79,10 +74,10 @@ cmp.setup({
   mapping = {
     -- ['<C-y>'] = cmp.mapping.confirm({ select = false }),
     ["<Tab>"] = cmp.mapping(function(fallback)
-      if luasnip.expand_or_locally_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
+      if cmp.visible() then
         cmp.confirm({select = false})
+      elseif luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
       else
         fallback()
       end
