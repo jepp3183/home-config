da0baad875700c6e57cee2c854e9df1e66936ed3
Jeppe Allerslev <jeppeallerslev@gmail.com>
2024-11-22 12:49:46 +0100

    update vscode settings

--- configs/vscode.nix
+++ configs/vscode.nix
@@ -4,11 +4,18 @@
     enable = true;
     userSettings = {
       "window.titleBarStyle" = "custom";
+      "editor.fontFamily" = "CaskaydiaCove Nerd Font Mono";
+      "terminal.integrated.fontFamily" = "CaskaydiaCove Nerd Font Mono";
+      "editor.inlayHints.enabled" = "offUnlessPressed";
+      "rust-analyzer.checkOnSave.command" = "clippy";
+      "workbench.colorTheme" = "Ayu Mirage";
     };
     extensions = with pkgs.vscode-extensions; [
       ms-vsliveshare.vsliveshare
       github.copilot
       github.copilot-chat
+      rust-lang.rust-analyzer
+      teabyii.ayu
     ];
   };
 }
