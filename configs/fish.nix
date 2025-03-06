5c28c1bb0de012cdfd34ff4ef3e4b7da0aa4810d
Jeppe Allerslev <jeppe.allerslev@qarmainspect.com>
2025-03-06 10:11:16 +0100

    auto rename zellij tabs

--- configs/fish.nix
+++ configs/fish.nix
@@ -116,6 +116,26 @@ in
           cd $choice
         end
       end
+
+    function zellij_update_tabname
+        if set -q ZELLIJ
+            set current_dir $PWD
+            if test $current_dir = $HOME
+                set current_dir "~"
+            else
+                set current_dir (basename $current_dir)
+            end
+            nohup zellij action rename-tab $current_dir >/dev/null 2>&1
+        end
+    end
+
+    # auto update tab name on directory change
+    function __auto_zellij_update_tabname --on-variable PWD --description "Update zellij tab name on directory change"
+        zellij_update_tabname
+    end
+    
+    # Update on start as well
+    zellij_update_tabname
     '';
   };
 }
