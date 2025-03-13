b22c1f1f6cfaba33448e10f53cd74d29e9274961
Jeppe Allerslev <jeppe.allerslev@qarmainspect.com>
2025-03-13 16:47:45 +0100

    add mapping descriptions

--- configs/neovim/mappings.lua
+++ configs/neovim/mappings.lua
@@ -22,12 +22,12 @@ vim.keymap.set('n', '<C-q>', MiniBufremove.delete, {desc="Close buffer"})
 
 -- Harpoon
 local harpoon = require('harpoon')
-vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
-vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<leader>h3", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<leader>h4", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = "Harpoon: Add file" })
+vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon: Toggle menu" })
+vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end, { desc = "Harpoon: Go to file 1" })
+vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end, { desc = "Harpoon: Go to file 2" })
+vim.keymap.set("n", "<leader>h3", function() harpoon:list():select(3) end, { desc = "Harpoon: Go to file 3" })
+vim.keymap.set("n", "<leader>h4", function() harpoon:list():select(4) end, { desc = "Harpoon: Go to file 4" })
 
 -- -- Folding
 -- local function cond_fold()
