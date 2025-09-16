d55e88dd89ce91142d5792821eed9be85e18123d
Jeppe Allerslev <jeppe.allerslev@qarmainspect.com>
2025-09-16 13:58:56 +0200

    Revert "remove harpoon"
    This reverts commit 54c511fe156c5a8f3d774e08702a96f0cdb0b3dc.

--- configs/neovim/default.nix
+++ configs/neovim/default.nix
@@ -61,6 +61,7 @@ in
     { plugin = gitsigns-nvim; config = toLua /* lua */ ''require("gitsigns").setup()''; }
     { plugin = neogit; config = toLua /* lua */ ''require("neogit").setup()''; }
     { plugin = yazi-nvim; config = toLua /* lua */ ''require("yazi").setup()''; }
+    { plugin = harpoon2; config = toLua /* lua */ ''require("harpoon").setup()'';}
 
     neotest-elixir
     { plugin = neotest; config = toLua /* lua */ ''
