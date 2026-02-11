dafec24054392b6ae218406c6e02e9f660ce1f9a
Jeppe Allerslev <jeppeallerslev@gmail.com>
2026-02-11 21:43:58 +0100

    fix snippet <tab> behaviour

--- configs/neovim/snippets.lua
+++ configs/neovim/snippets.lua
@@ -20,6 +20,18 @@ local types = require("luasnip.util.types")
 local conds = require("luasnip.extras.conditions")
 local conds_expand = require("luasnip.extras.conditions.expand")
 
+vim.api.nvim_create_autocmd('ModeChanged', {
+  pattern = '*',
+  callback = function()
+    if ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
+        and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
+        and not require('luasnip').session.jump_active
+    then
+      require('luasnip').unlink_current()
+    end
+  end
+})
+
 
 ls.add_snippets("nix", {
     s("plugin",
