2d78b2e7a25835561fe4150a9990b25fca5ba5a0
Jeppe Allerslev <jeppe.allerslev@qarmainspect.com>
2026-02-13 14:45:52 +0100

    fix indentation frfr

--- configs/neovim/basics.lua
+++ configs/neovim/basics.lua
@@ -7,8 +7,8 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
+-- See :help c-indenting
+vim.opt.cindent = true
 vim.opt.cinkeys:remove("0#") -- Fix comments outdenting in elixir
 vim.opt.incsearch = true
 vim.opt.termguicolors = true
