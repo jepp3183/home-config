f081931f13c206f20aeed11aee6b813804f27a3f
Jeppe Allerslev <jeppeallerslev@gmail.com>
2026-03-30 22:05:18 +0200

    remove light control from streamdeck, fix speaker sink

--- configs/streamdeck_ui.nix
+++ configs/streamdeck_ui.nix
@@ -11,48 +11,6 @@
             "AL49J2C03127": {
                 "buttons": {
                     "0": {
-                        "0": {
-                            "state": 0,
-                            "states": {
-                                "0": {
-                                    "text": "Dec",
-                                    "icon": "",
-                                    "keys": "",
-                                    "write": "",
-                                    "command": "http POST 192.168.0.169:5000/decrement?step=25",
-                                    "brightness_change": 0,
-                                    "switch_page": 0,
-                                    "switch_state": 0,
-                                    "text_vertical_align": "middle",
-                                    "text_horizontal_align": "",
-                                    "font": "",
-                                    "font_color": "",
-                                    "font_size": 0,
-                                    "background_color": ""
-                                }
-                            }
-                        },
-                        "1": {
-                            "state": 0,
-                            "states": {
-                                "0": {
-                                    "text": "Inc",
-                                    "icon": "",
-                                    "keys": "",
-                                    "write": "",
-                                    "command": "http POST 192.168.0.169:5000/increment?step=25",
-                                    "brightness_change": 0,
-                                    "switch_page": 0,
-                                    "switch_state": 0,
-                                    "text_vertical_align": "middle",
-                                    "text_horizontal_align": "",
-                                    "font": "",
-                                    "font_color": "",
-                                    "font_size": 0,
-                                    "background_color": ""
-                                }
-                            }
-                        },
                         "2": {
                             "state": 0,
                             "states": {
@@ -82,7 +40,7 @@
                                     "icon": "",
                                     "keys": "",
                                     "write": "",
-                                    "command": "pactl set-default-sink alsa_output.pci-0000_28_00.4.analog-stereo",
+                                    "command": "pactl set-default-sink alsa_output.pci-0000_26_00.1.hdmi-stereo-extra1",
                                     "brightness_change": 0,
                                     "switch_page": 0,
                                     "switch_state": 0,
@@ -104,85 +62,6 @@
                                     "keys": "",
                                     "write": "",
                                     "command": "pactl set-default-sink alsa_output.usb-FiiO_DigiHug_USB_Audio-01.analog-stereo",
-                                    "brightness_change": 0,
-                                    "switch_page": 0,
-                                    "switch_state": 0,
-                                    "text_vertical_align": "middle",
-                                    "text_horizontal_align": "",
-                                    "font": "",
-                                    "font_color": "",
-                                    "font_size": 0,
-                                    "background_color": ""
-                                }
-                            }
-                        },
-                        "5": {
-                            "state": 0,
-                            "states": {
-                                "0": {
-                                    "text": "Toggle",
-                                    "icon": "",
-                                    "keys": "",
-                                    "write": "",
-                                    "command": "http POST 192.168.0.169:5000/toggle",
-                                    "brightness_change": 0,
-                                    "switch_page": 0,
-                                    "switch_state": 0,
-                                    "text_vertical_align": "middle",
-                                    "text_horizontal_align": "",
-                                    "font": "",
-                                    "font_color": "",
-                                    "font_size": 0,
-                                    "background_color": ""
-                                }
-                            }
-                        },
-                        "6": {
-                            "state": 0,
-                            "states": {
-                                "0": {
-                                    "text": "Dim",
-                                    "icon": "",
-                                    "keys": "",
-                                    "write": "",
-                                    "command": "http POST 192.168.0.169:5000/set temp==2700 brightness==80",
-                                    "brightness_change": 0,
-                                    "switch_page": 0,
-                                    "switch_state": 0,
-                                    "text_vertical_align": "middle",
-                                    "text_horizontal_align": "",
-                                    "font": "",
-                                    "font_color": "",
-                                    "font_size": 0,
-                                    "background_color": ""
-                                },
-                                "1": {
-                                    "text": "",
-                                    "icon": "",
-                                    "keys": "",
-                                    "write": "",
-                                    "command": "",
-                                    "brightness_change": 0,
-                                    "switch_page": 0,
-                                    "switch_state": 0,
-                                    "text_vertical_align": "",
-                                    "text_horizontal_align": "",
-                                    "font": "",
-                                    "font_color": "",
-                                    "font_size": 0,
-                                    "background_color": ""
-                                }
-                            }
-                        },
-                        "7": {
-                            "state": 0,
-                            "states": {
-                                "0": {
-                                    "text": "Full",
-                                    "icon": "",
-                                    "keys": "",
-                                    "write": "",
-                                    "command": "http POST 192.168.0.169:5000/set temp==2700 brightness==255",
                                     "brightness_change": 0,
                                     "switch_page": 0,
                                     "switch_state": 0,
