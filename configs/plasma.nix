6a5ddd9175751794da28a38b08bd5f3bf3508719
Jeppe Allerslev <jeppeallerslev@gmail.com>
2026-03-30 21:57:20 +0200

    add plasma shortcuts

--- configs/plasma.nix
+++ configs/plasma.nix
@@ -12,6 +12,36 @@
       repeatRate = 40;
     };
 
+    hotkeys.commands = {
+      kitty = {
+        name = "Launch kitty";
+        key = "Meta+Return";
+        command = "kitty";
+      };
+      firefox = {
+        name = "Firefox";
+        key = "Meta+B";
+        command = "firefox";
+      };
+    };
+
+    shortcuts = {
+      kwin = {
+        "Window Maximize" = [
+          "Meta+F"
+          "Meta+PgUp"
+        ];
+        "Window Close" = [
+          "Alt+F4"
+          "Meta+Q"
+        ];
+      };
+
+      # Defaults removed:
+      org_kde_powerdevil.powerProfile = ["Battery"];
+      plasmashell."manage activities" = [ ];
+    };
+
     configFile = {
       kxkbrc.Layout.Options = "grp:win_space_toggle,caps:ctrl_modifier";
     };
